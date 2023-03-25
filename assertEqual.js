07b519938aa448e07944b4aa8e2233920e5906fd
TinyMito <k.ip@me.com>
2023-03-24 21:10:08 -0400

    Updated with template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log('\u{1F600} Assertion Passed: ' + [actual] + ' === ' + [expected]);
+    console.log(`\u{1F600} Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log('\u{1F622} Assertion Failed: ' + [actual] + ' !== ' + [expected]);
+    console.log(`\u{1F622} Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
