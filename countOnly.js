8d21ae7ad77847a2613684b6979158c53107af76
TinyMito <kip@me.com>
2023-03-31 01:20:34 -0400

    New

--- countOnly.js
+++ countOnly.js
@@ -6,8 +6,6 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// allItems: an array of strings that we need to look through
-// itemsToCount: an object specifying what to count
 const countOnly = function(allItems, itemsToCount) {
   const results = {}
 
@@ -20,7 +18,6 @@ const countOnly = function(allItems, itemsToCount) {
       }
     }
   }
-
   return results;
 }
 
