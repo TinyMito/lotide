1a604ec41e7a5841141f3ed99703998ffa5d56f2
TinyMito <k.ip@me.com>
2023-04-02 01:26:22 -0400

    Updated

--- map.js
+++ map.js
@@ -20,7 +20,6 @@ const assertArraysEqual = function(arrayData1, arrayData2) {
 };
 
 const words = ["ground", "control", "to", "major", "tom"];
-
 const map = function(array, callback) {
   const results = [];
   for (let item of array) {
