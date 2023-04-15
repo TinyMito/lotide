01a8961dc787b79c0c98210d4f104a95c2f1881e
TinyMito <k.ip@me.com>
2023-04-15 09:20:31 -0400

    Added comments

--- flatten.js
+++ flatten.js
@@ -1,12 +1,16 @@
 const flatten = function(inputArray) {
+  // Create a new array to output
   let newArray = [];
 
   for (let a = 0; a < inputArray.length; a++) {
     if (Array.isArray(inputArray[a])) {
+      // Condition if the element is an Array itself
       for (let b = 0; b < inputArray[a].length; b++) {
+        // Push each element into the newArray
         newArray.push(inputArray[a][b]);
       }
     } else {
+      // Else just push the element straight into newArray
       newArray.push(inputArray[a]);
     }
   }
