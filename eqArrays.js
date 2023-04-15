59da30d73187a54388b8ea6910ed6318be9de8f8
TinyMito <k.ip@me.com>
2023-04-15 09:00:37 -0400

    Added comments

--- eqArrays.js
+++ eqArrays.js
@@ -1,20 +1,24 @@
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
-    return false; // Return immediately false of array length not matched.
+    // Return immediately false of array length not matched.
+    return false; 
   };
 
   for (let i in array1) {
+    // Condidion if the element is an Array itself
     if (Array.isArray(array1[i])) {
       if (!eqArrays(array1[i], array2[i])) {
-      // Recursive
+      // Recursive the element to check within again
         return false;
       }
     } else if (array1[i] !== array2[i]) {
+      // Condition return false if the element doesn't match
       return false;
     }
   };
 
-return true; // If loop did not find any false then return true!
+// If loop did not find any false then return true!
+return true; 
 };
 
 module.exports = eqArrays;
