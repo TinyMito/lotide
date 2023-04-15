a32ef7b5ea6d5434682fb09cd26c5d6ba6c103db
TinyMito <k.ip@me.com>
2023-04-15 09:26:28 -0400

    Cleanup code

--- map.js
+++ map.js
@@ -1,9 +1,12 @@
 const map = function(array, callback) {
+  // New array
   const results = [];
+
   for (let item of array) {
+    // Callback function
     results.push(callback(item));
   }
   return results;
-}
+};
 
 module.exports = map;
