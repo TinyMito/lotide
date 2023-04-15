580dff8c9d423d756aa9cc8bb15d23d795e24c89
TinyMito <k.ip@me.com>
2023-04-15 01:10:22 -0400

    Updated eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -4,31 +4,22 @@ const eqArrays = function(a,b) {
   };
 
   for (let i in a) {
-    console.log(a[i])
     if (Array.isArray(a[i])) {
-      console.log('Array!')
-      if (!Array.isArray(a[i], b[i])) {
-        console.log('false')
+      if (!eqArrays(a[i], b[i])) {
+      // Recursive
         return false;
       }
     } else if (a[i] !== b[i]) {
-      console.log('false')
       return false;
     }
   };
 
-/* for (let i = 0; i < a.length; i++) {
-  if (a[i] !== b[i]) { // Finds the bad sheep!
-    return false;
-  }
-} */
 return true; // If loop did not find any false then return true!
 };
 
-//eqArrays([[2, 3], [4]], [[2, 3], [4]]); // => true
-
+eqArrays([[2, 3], [4]], [[2, 3], [4]]); // => true
 eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]) // => false
-//eqArrays([[2, 3], [4]], [[2, 3], 4]) // => false
+eqArrays([[2, 3], [4]], [[2, 3], 4]) // => false
 
 
 module.exports = eqArrays;
--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -3,12 +3,12 @@ const eqArrays = require('../eqArrays');
 
 describe("#eqArrays", () => {
   it("returns true", () => {
-    assert.strictEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+    assert.strictEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
   });
   it("returns false", () => {
-    assert.strictEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+    assert.strictEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
   });
   it("returns false", () => {
-    assert.strictEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+    assert.strictEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
   });
 });
