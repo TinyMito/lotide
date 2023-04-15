3ff17efd89ecbd565e336d062a3aa76308cd474a
TinyMito <k.ip@me.com>
2023-04-15 09:24:09 -0400

    Cleanup

--- letterPositions.js
+++ letterPositions.js
@@ -1,12 +1,22 @@
 const letterPositions = function(sentence) {
-  const results = {}; // Declare object
-  for (let i = 0; i < sentence.length; i++) { // Traditional loop to determine the index of each letter for of will not work on duplicated value
-    const letter = sentence[i]; // Each letter into string
-    if (letter !== " ") { // If it is not a empty space
-      if (results[letter] === undefined) { // If the letter doesn't exist in the object key
-        results[letter] = [i]; // Add key as array
+  // Declare object
+  const results = {};
+
+  for (let i = 0; i < sentence.length; i++) {
+    // Traditional loop to determine the index of each letter for of will
+    // not work on duplicated value
+    const letter = sentence[i];
+    // Each letter into string
+    if (letter !== " ") {
+      // If it is not a empty space
+      if (results[letter] === undefined) {
+        // If the letter doesn't exist in the object key
+        // Add key as array
+        results[letter] = [i];
+        
       } else {
-        results[letter].push(i); // If not push a value into the array
+        // If not push a value into the array
+        results[letter].push(i);
       }
     }
   }
