36a480cfaec96c6076a1f8db79fd15ecbbac67a6
TinyMito <k.ip@me.com>
2023-04-15 09:03:03 -0400

    Updated: Comments

--- eqObjects.js
+++ eqObjects.js
@@ -1,14 +1,14 @@
 const eqObjects = function(object1, object2) {
+  // Return false if both object1 and object2 length unequal
   if (Object.keys(object1).length !== Object.keys(object2).length) { 
     return false;
   };
 
   for (const key of Object.keys(object1)) { 
-    //console.log(object1[key])
+    // Check condition if it is an object
     if (typeof object1[key] === 'object' && !Array.isArray(object2[key]) && object1[key] !== null) { 
-    // If the property is an object.
       if (!eqObjects(object1[key], object2[key])) {
-      // Recursive eqObjects(), if return false.
+      // Recursive eqObjects(), if return false
         return false;
       }
     } else if (object1[key] !== object2[key]) {
@@ -17,6 +17,7 @@ const eqObjects = function(object1, object2) {
     }
   }
 
+  // If loop did not find any false then return true!
   return true;
 };
 
