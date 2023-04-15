fac3b97cacc94238832c8b8fb773165551a54258
TinyMito <k.ip@me.com>
2023-04-15 08:35:23 -0400

    Improved description

--- test/countOnlyTest.js
+++ test/countOnlyTest.js
@@ -16,16 +16,16 @@ const firstNames = [
 const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
 
 describe("#countOnly", () => {
-  it("returns 1", () => {
+  it("Count only 'Jason', returns 1", () => {
     assert.strictEqual(result1["Jason"], 1);
   });
-  it("returns undefined", () => {
+  it("Count only 'Karima', returns undefined", () => {
     assert.strictEqual(result1["Karima"], undefined);
   });
-  it("returns 2", () => {
+  it("Count only 'Fang', returns 2", () => {
     assert.strictEqual(result1["Fang"], 2);
   });
-  it("returns undefined", () => {
+  it("Count only 'Agouhanna', returns undefined", () => {
     assert.strictEqual(result1["Agouhanna"], undefined);
   });
 });
