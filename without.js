72d98022b90a5a4474477b8cc1f1f0fbcb4f4ad7
TinyMito <k.ip@me.com>
2023-04-15 09:31:08 -0400

    cleaned up codes

--- without.js
+++ without.js
@@ -1,10 +1,15 @@
 const without = function(inputArray, removeElement) {
-  let newArray = []; // To make sure it is non-destructive of the original array.
+  // To make sure it is non-destructive of the original array.
+  let newArray = [];
+
   for (let i = 0; i < inputArray.length; i++) {
-    let doRemove = false; // A flag to prevent trigger removal function
-    for (let x = 0; x < removeElement.length; x++) { // Since the 2nd element is an array itself, needs to for loop.
-      if (inputArray[i] === removeElement[x]) { 
-        doRemove = true; // If matched, perform doRemove [i] element.
+    // A flag to prevent trigger removal function
+    let doRemove = false;
+    for (let x = 0; x < removeElement.length; x++) {
+      // Since the 2nd element is an array itself, needs to for loop.
+      if (inputArray[i] === removeElement[x]) {
+        // If matched, perform doRemove [i] element.
+        doRemove = true;
       }
     }
     if (!doRemove) {
