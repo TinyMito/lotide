3d9d87c20f619dd057851ef07137fee8b19fd64c
TinyMito <k.ip@me.com>
2023-04-15 01:29:00 -0400

    Clean up and fix

--- eqArrays.js
+++ eqArrays.js
@@ -1,25 +1,20 @@
-const eqArrays = function(a,b) {
-  if (a.length !== b.length) {
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
     return false; // Return immediately false of array length not matched.
   };
 
-  for (let i in a) {
-    if (Array.isArray(a[i])) {
-      if (!eqArrays(a[i], b[i])) {
+  for (let i in array1) {
+    if (Array.isArray(array1[i])) {
+      if (!eqArrays(array1[i], array2[i])) {
       // Recursive
         return false;
       }
-    } else if (a[i] !== b[i]) {
+    } else if (array1[i] !== array2[i]) {
       return false;
     }
   };
 
 return true; // If loop did not find any false then return true!
 };
-
-eqArrays([[2, 3], [4]], [[2, 3], [4]]); // => true
-eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]) // => false
-eqArrays([[2, 3], [4]], [[2, 3], 4]) // => false
-
 
 module.exports = eqArrays;
