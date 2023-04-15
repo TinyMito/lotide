bc89a4be7e6975da971d014cc76f9490897b2dbd
TinyMito <k.ip@me.com>
2023-04-15 08:26:18 -0400

    Changed: let to const for middleArr

--- middle.js
+++ middle.js
@@ -1,15 +1,16 @@
 const middle = function(inputArray) {
   if (inputArray.length < 3) {
+    // If the array.length is less than two elements, return empty
     return [];
   }
 
   if (inputArray.length % 2 === 0) {
-    // Even numbers does something
-    let middleArr = Math.floor(inputArray.length / 2);
+    // If the array.length has an even number, output the two elements
+    const middleArr = Math.floor(inputArray.length / 2);
     return inputArray.slice(middleArr - 1, middleArr + 1);
   } else {
-    // Odd numbers does something
-    let middleArr = Math.floor(inputArray.length / 2);
+    // Else would be consider an odd number, output the one element
+    const middleArr = Math.floor(inputArray.length / 2);
     return inputArray.slice(middleArr, middleArr + 1);
   }
 };
