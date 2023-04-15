bfeafc07d3233c623709b953f07f96c6db540ce4
TinyMito <k.ip@me.com>
2023-04-15 08:48:39 -0400

    Added comments

--- countOnly.js
+++ countOnly.js
@@ -1,8 +1,13 @@
 const countOnly = function(allItems, itemsToCount) {
+  // Create new object to store the count of each element
   const results = {}
 
   for (const item of allItems) {
+    // Run this condition if the key is true
     if (itemsToCount[item]) {
+      // Condition check if the key existed in the result object,
+      // if existed; +1 count and if not initial the first count 1
+      console.log(item)
       if (results[item]) {
         results[item] += 1;
       } else {
@@ -10,6 +15,7 @@ const countOnly = function(allItems, itemsToCount) {
       }
     }
   }
+
   return results;
 }
 
