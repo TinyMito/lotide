a2f890b094c3356c7e8cc83b070580aa927dd8a5
TinyMito <k.ip@me.com>
2023-04-15 21:19:28 -0400

    Added assertArraysEqual, assertObjectsEqual, eqArrays, eqObjects, assertEqual

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 // index.js
-const countLetters      = require('./countLetters');
-const countOnly         = require('./countOnly');
-const findKey           = require('./findKey');
-const findKeybyValue    = require('./findKeybyValue');
-const flatten           = require('./flatten');
-const head              = require('./head');
-const letterPositions   = require('./letterPositions');
-const map               = require('./map');
-const middle            = require('./middle');
-const tail              = require('./tail');
-const takeUntil         = require('./takeUntil');
-const without           = require('./without');
+const assertArraysEqual   = require('./assertArraysEqual');
+const assertEqual         = require('./assertEqual');
+const assertObjectsEqual  = require('./assertObjectsEqual');
+const countLetters        = require('./countLetters');
+const countOnly           = require('./countOnly');
+const eqArrays            = require('./eqArrays');
+const eqObjects           = require('./eqObjects');
+const findKey             = require('./findKey');
+const findKeybyValue      = require('./findKeybyValue');
+const flatten             = require('./flatten');
+const head                = require('./head');
+const letterPositions     = require('./letterPositions');
+const map                 = require('./map');
+const middle              = require('./middle');
+const tail                = require('./tail');
+const takeUntil           = require('./takeUntil');
+const without             = require('./without');
 
 module.exports = {
+  assertArraysEqual,
+  assertObjectsEqual,
   countLetters,
   countOnly,
+  eqArrays,
+  eqObjects,
   findKey,
   findKeybyValue,
   flatten,
