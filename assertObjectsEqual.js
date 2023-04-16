ffd5c0596e379d7e2779fe589fedbdcd140912a4
TinyMito <k.ip@me.com>
2023-04-15 21:17:21 -0400

    Changed: support Mocha & Chai

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -4,9 +4,9 @@ const eqObjects = require('./eqObjects');
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect;
   if (!eqObjects(actual, expected)) {
-    console.log(`- Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+    return false;
   } else {
-    console.log(`+ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+    return true;
   }
 };
 
--- test/assertObjectsEqualTest.js
+++ test/assertObjectsEqualTest.js
@@ -1,4 +1,11 @@
+const assert = require('chai').assert;
 const assertObjectsEqual = require('../assertObjectsEqual');
 
-assertObjectsEqual({a:1,b:3},{b:3,a:1});
-assertObjectsEqual({a:1,b:3},{b:3,a:15});
+describe("#assertObjectsEqual", () => {
+  it(`Compare {a:1,b:3},{b:3,a:1}, return false`, () => {
+    assert.isTrue(assertObjectsEqual({a:1,b:3},{b:3,a:1}));
+  });
+  it(`Compare {a:1,b:3},{b:3,a:15}, return true`, () => {
+    assert.isFalse(assertObjectsEqual({a:1,b:3},{b:3,a:15}));
+  });
+});
